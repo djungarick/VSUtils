51efd0515dd3c7ec12dd328256c1f79a747ffee3
djungarick <145795588+djungarick@users.noreply.github.com>
2023-09-23 11:04:06 +0300

    feat(ReplaceItemTemplates): Add replacement classes and interfaces

--- ReplaceItemTemplates/Program.cs
+++ ReplaceItemTemplates/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Runtime.InteropServices;
+using System.Text;
+
+const string ClassTemplate = """
+    namespace $rootnamespace$;
+
+    internal sealed class $safeitemrootname$
+    {
+        
+    }
+
+    """;
+const string InterfaceTemplate = """
+    namespace $rootnamespace$;
+
+    public interface $safeitemrootname$
+    {
+        
+    }
+
+    """;
+
+if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+    ContinueUnderWindows();
+else
+    throw new NotSupportedException("Only OS 'Windows' is supported currently.");
+
+static void ContinueUnderWindows()
+{
+    const string BasePathTemplate = @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\ItemTemplates\{0}\Code\1033";
+    string[] folders = { "AspNetCore", "CSharp" };
+
+    string classPathTemplate = Path.Combine(BasePathTemplate, "Class", "Class.cs");
+    string interfacePathTemplate = Path.Combine(BasePathTemplate, "Interface", "Interface.cs");
+
+    foreach (string folder in folders)
+    {
+        using (FileStream classFileStream = File.Open(string.Format(classPathTemplate, folder), FileMode.Truncate, FileAccess.Write))
+        {
+            classFileStream.Write(Encoding.UTF8.GetBytes(ClassTemplate));
+        }
+
+        using (FileStream interfaceFileStream = File.Open(string.Format(interfacePathTemplate, folder), FileMode.Truncate, FileAccess.Write))
+        {
+            interfaceFileStream.Write(Encoding.UTF8.GetBytes(InterfaceTemplate));
+        }
+    }
+}
