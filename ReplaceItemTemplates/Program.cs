68077e4ca64eddb9b9865f28c8a03cc1e0691625
Roman Volkov <roman.volkov.kz@outlook.com>
2024-07-20 17:06:37 +0300

    feat: update to .net8

--- ReplaceItemTemplates/Program.cs
+++ ReplaceItemTemplates/Program.cs
@@ -17,14 +17,12 @@ void ContinueUnderWindows()
     string[] interfacePaths =
     {
         @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\ItemTemplates\AspNetCore\Code\1033\Interface\Interface.cs",
-        @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\ItemTemplates\CSharp\Code\1033\Interface\Interface.cs",
-        @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\ItemTemplates\CSharp\Code\1049\Interface\Interface.cs"
+        @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\ItemTemplates\CSharp\Code\1033\Interface\Interface.cs"
     };
     string[] classPaths =
     {
         @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\ItemTemplates\AspNetCore\Code\1033\Class\Class.cs",
-        @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\ItemTemplates\CSharp\Code\1033\Class\Class.cs",
-        @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\ItemTemplates\CSharp\Code\1049\Class\Class.cs"
+        @"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\ItemTemplates\CSharp\Code\1033\Class\Class.cs"
     };
 
     foreach (string classPath in classPaths)
